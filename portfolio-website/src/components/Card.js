7fbeec1c5656e8d23c8ae16e569b4f2af001f524
Rishi Raj <rraj7@uic.edu>
2020-08-31 01:08:13 -0500

    Added home page

--- portfolio-website/src/components/Card.js
+++ portfolio-website/src/components/Card.js
@@ -5,7 +5,7 @@ import CardInfo from './CardInfo';
 function Card(props){
 
     return (
-        <div className='d-inline-block r-card' onClick = {(e)=> props.click(props.item)}>
+        <div className='d-inline-block r-card' click = {(e)=> props.click(props.item)}>
             <img className = "r-card-img " src ={props.item.imgSrc} alt = {props.item.imgSrc}/>
             { props.item.selected && <CardInfo title= {props.item.title} subtitle={props.item.subtitle} link ={props.item.link}/> }
         </div>
